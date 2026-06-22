cadbd45bcc745cbae65e7e33f459e4391b57d651
MagsnapMZ <MagsnapMZ@users.noreply.github.com>
2026-06-22 18:39:21 +0700

    replace founder copy with panda masters

--- assets/founder/registry-data.js
+++ assets/founder/registry-data.js
@@ -3,7 +3,7 @@ window.MAGSNAP_REGISTRY_SEED = [
     registry_number: "0171",
     display_name: "MZ",
     country: "China",
-    role: "Founder",
+    role: "Panda Master",
     configuration: "Type A / Blue Lens",
     status: "Origin"
   }
